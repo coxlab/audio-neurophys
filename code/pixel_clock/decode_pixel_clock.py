07ea31b10b271277ad8dec1c06c4d1ea605ca596
David Cox <david.daniel.cox@gmail.com>
2010-09-14 15:22:39 -0400

    Incremental edits

--- code/pixel_clock/decode_pixel_clock.py
+++ code/pixel_clock/decode_pixel_clock.py
@@ -16,6 +16,15 @@ velocity_threshold = 0.025
 positive_transitions = []
 negative_transitions = []
 
+def reduce_runs(l):
+    last = -2
+    out = []
+    for i in l:
+        if i != last + 1:
+            out.append(i)
+        last = i
+    return out    
+
 for c in range(0,4):
 
     filepath = os.path.join(project_path, "%s%d#%.2d.wav" % (pixclock_prefix, c+1, recording_number))
@@ -27,19 +36,32 @@ for c in range(0,4):
     wav_velocity = diff(wav_data)
     smooth_wav_velocity = convolve(wav_velocity, ones((100)))
 
-    positive_transitions.append(where(smooth_wav_velocity > velocity_threshold)[0])
-    negative_transitions.append(where(smooth_wav_velocity < -velocity_threshold)[0])
+    plt.plot(smooth_wav_velocity[0:-1:100])
+    plt.show()
+
+    print max(smooth_wav_velocity)
+    print min(smooth_wav_velocity)
+
+    positive_transitions.append(reduce_runs(where(smooth_wav_velocity > velocity_threshold)[0]))
+    negative_transitions.append(reduce_runs(where(smooth_wav_velocity < -velocity_threshold)[0]))
+
+
+    print("n items = %d" % (len(positive_transitions[-1])))
 
 plt.figure()
 plt.hold(True)
 
-for c in range(0,4):
+print(positive_transitions)
+
+for c in range(0,len(positive_transitions)):
     for t in positive_transitions[c]:
+        print("%d,%d +" % (c,t))
         plt.plot(c, t, '+')
     
     for t in negative_transitions[c]:
         plt.plot(c, t, 'o')
-        
+
+print positive_transitions
     
 plt.show()
 
